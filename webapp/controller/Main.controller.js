1d2dd28f4522af79fccb89225f11b15b82026bca
P2000959701 <yorick.scheyltjens@gmail.com>
2018-11-29 12:03:18 +0000

    Added code for grouping data

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -14,42 +14,36 @@ sap.ui.define([
 		onInit: function () {
 			this.getIotData();
 		},
-
 		getIotData: function () {
 			// url to get the artifact signals of your device : 
 			// '/devices/XX/measures'  -> XX = your device id
-			
-			var promise = new Promise(function (resolve, reject) {
-				$.ajax({
-					type: "GET",
-					url: "/devices/102/measures",
-					headers: "",
-					success: function (data) {
-						console.log(data);
-						resolve(data);
-					},
-					error: function (Error) {
-						reject((Error));
-					},
-					contentType: false,
-					async: true,
-					data: null,
-					cache: false,
-					processData: false
-				});
+			var me = this;
+			$.ajax({
+				url: "/devices/102/measures",
+				success: function (result) {
+					me.groupData(result);
+				}
 			});
-			return Promise.resolve(promise).then(function (result) {
-				return "Bearer " + result.access_token;
-			});
+		},
+
+		groupData: function (iotData) {
+			var object = [];
+			for(var i = 0; i < iotData.length; i+=4)
+			{
+				var item = 
+				{
+					artifact_id:iotData[i].measure.artifact_id,
+					longitude:iotData[i+1].measure.longitude,
+					latitude:iotData[i+2].measure.latitude,
+					artifact_signal:iotData[i+3].measure.artifact_signal
+				};
+				object.push(item);
+			}
 			
 			
 		},
 
-		groupData: function () {
-		},
-
-		triggerML: function (oEvent) {
-		},
+		triggerML: function (oEvent) {},
 
 		getMlAuthToken: function () {
 			var promise = new Promise(function (resolve, reject) {
@@ -77,14 +71,13 @@ sap.ui.define([
 		},
 
 		sendToMl: function () {
-		
+
 			//Use the following format to send to ML (image name can always be 'ArtifactSignal.jpg')
 			//image is a variable
 			//var formData = new FormData();
 			//formData.append("files", image, "ArtifactSignal.jpg");
-			
+
 			//url to post on : '/ml-dest/api/v2/image/classification/models/HTF/versions/2'
-			
 
 		},
 
@@ -116,4 +109,3 @@ sap.ui.define([
 
 	});
 });
-
